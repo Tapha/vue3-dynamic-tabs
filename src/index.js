c251789b61d8b13c7c04c14e290dc7d8d836bf95
Tapha Ngum <tapha@live.co.uk>
2022-01-20 04:22:09 +0000

    initial set of changes to modify new tabs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Tab from './components/Tab.vue';
 import Tabs from './components/Tabs.vue';
+import DynamicTab from './components/DynamicTab.vue';
+import DynamicTabContent from './components/DynamicTabContent.vue';
 
 export default {
     install(Vue) {
         Vue.component('tab', Tab);
         Vue.component('tabs', Tabs);
+        Vue.component('dynamic-tab', DynamicTab);
+        Vue.component('dynamic-tab-content', DynamicTabContent);
     },
 };
 
-export { Tab, Tabs };
+export { Tab, Tabs, DynamicTab, DynamicTabContent };
